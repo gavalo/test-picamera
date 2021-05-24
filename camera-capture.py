3d24669827ffe630d82d2d0ffecfba94d4b66e0a
gavalo <ametepe-gavalo.ntsouglo.1@ens.etsmtl.ca>
2021-05-24 13:31:12 -0400

    update

--- camera-capture.py
+++ camera-capture.py
@@ -8,7 +8,7 @@ time.sleep(2)
 #camera.resolution = (1280, 720)
 
 # Try maximum resolution (supported by this camera module)
-#camera.resolution = (2592, 1944)
+camera.resolution = (2592, 1944)
 
 file_name = "/home/pi/test-picamera/images/img_" + str(time.time()) + ".jpg"
 camera.capture(file_name)
